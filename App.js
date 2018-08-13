025eda07b9457b3d94824b8233f4ab27e5baebbe
Anuraag <Anuraag0551@gmail.com>
2018-08-13 06:37:01 +0530

    Basics

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
+//imports
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {View,Text} from 'react-native';
 
-export default class App extends React.Component {
+//create 
+class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={{flex = 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Hello World , this is reatc native</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+//exports
+exports default App;
